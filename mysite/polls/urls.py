204f6839c2c8a85d058e2720f58b9e3a86b8489c
Gabryss <zeratul312@gmail.com>
2020-10-05 17:10:23 +0200

    Add votes and resutl

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name = 'index'),
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('<int:question_id>/results/',views.result,name='results'),
 
     path('<int:question_id>/vote/',views.vote,name='vote'),
+
 ]
