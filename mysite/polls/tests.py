3bb6d4e2635cc11aa728e943b44004ff9db7b6bd
Gabryss <zeratul312@gmail.com>
2020-10-08 13:57:14 +0200

    Test bug fixing

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -48,7 +48,7 @@ class QuestionIndexViewTest(TestCase):
         """
         response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code,200)
-        self.assertContains(response, "test_no_questions - No polls are available.")
+        self.assertContains(response, "No polls are available.")
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
     
     def test_pass_question(self):
@@ -68,7 +68,7 @@ class QuestionIndexViewTest(TestCase):
         """
         create_question(question_text="Future question.", days=30)
         response = self.client.get(reverse('polls:index'))
-        self.assertContains(response, "test_future_question - No polls are available.")
+        self.assertContains(response, "No polls are available.")
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
     
     def test_future_question_and_past_question(self):
@@ -94,3 +94,22 @@ class QuestionIndexViewTest(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question 2.>','<Question: Past question 1.>']
         )
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future returns a 404 not found.
+        """
+        future_question = create_question(question_text='Future question', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+    
+    def test_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past display the question's text.
+        """
+        past_question = create_question(question_text= 'Past question', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response= self.client.get(url)
+        self.assertContains(response, past_question.question_text)
